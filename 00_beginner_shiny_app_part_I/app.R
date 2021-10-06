c93c4b3e3e6085673e9f66b48bf8ec3eb3bd0f61
Jesse Mostipak <jesse.maegan@gmail.com>
2021-10-05 19:05:03 -0500

    code for first tweetorial added

---
+++ 00_beginner_shiny_app_part_I/app.R
@@ -0,0 +1,49 @@
+# set up ----
+library(shiny)
+library(ggplot2)
+library(DT)
+library(palmerpenguins)
+
+# user interface ----
+ui <- fluidPage(
+  
+  sidebarLayout(
+    sidebarPanel(),
+    
+    mainPanel(
+      
+      # scatterplot (ui) ----
+      plotOutput(outputId = "penguin_scatterplot"),
+      
+      # datatable (ui) ----
+      DTOutput(outputId = "penguin_datatable"),
+      
+    )
+  )
+)
+
+# server function ----
+server <- function(input, output, session) {
+  
+  # scatterplot ----
+  
+  output$penguin_scatterplot <- renderPlot({
+    
+    ggplot(data = penguins,
+           aes(x = bill_length_mm, y = flipper_length_mm)) +
+      geom_point()
+    
+  })
+  
+  # datatable (server) ----
+  
+  output$penguin_datatable <- renderDT({
+    
+    DT::datatable(penguins)
+    
+  })
+  
+}
+
+# run Shiny app ----
+shinyApp(ui, server)
